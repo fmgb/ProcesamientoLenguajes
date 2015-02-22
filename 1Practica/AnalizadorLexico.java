8acb34bc418ecec9d0526b3edef0481c8c26d353
fmgb <fmgb92@gmail.com>
2015-02-22 22:36:42 +0100

    Organizado estructura de la asignatura. Iniciación de la primera práctica.

---
+++ 1Practica/AnalizadorLexico.java
@@ -0,0 +1,21 @@
+package 1Practica;
+
+class AnalizadorLexico
+{
+    private String _FilePath;
+    
+    public AnalizadorLexico()
+    {
+        _FilePath = "./Fichero.txt";
+    }
+
+    public AnalizadorLexico(String filePath)
+    {
+        _FilePath = filePath;
+    }
+
+    public void siguienteToken() //Return Token
+    {
+        
+    }
+}
